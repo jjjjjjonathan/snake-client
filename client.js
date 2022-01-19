6e3ce70e6d354daf7a5d73b2c74813fcb574b40c
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-19 14:39:53 -0500

    Added name to snake when connected to server

--- client.js
+++ client.js
@@ -4,11 +4,14 @@ const connect = function() {
     host: 'localhost',// IP address here,
     port: 50541// PORT number here,
   });
-
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
   
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: JMC')
+  })
+
   conn.setTimeout(5000);
   conn.on('timeout', () => {
     console.log("you ded cuz you idled");
--- play.js
+++ play.js
@@ -1,22 +1,4 @@
 const { connect } = require('./client')
-// establishes a connection with the game server
-// const connect = function() {
-//   const conn = net.createConnection({
-//     host: 'localhost',// IP address here,
-//     port: 50541// PORT number here,
-//   });
-
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-  
-//   conn.setTimeout(5000);
-//   conn.on('timeout', () => {
-//     console.log("you ded cuz you idled");
-//   });
-
-//   return conn;
-// };
 
 console.log("Connecting ...");
 connect();
