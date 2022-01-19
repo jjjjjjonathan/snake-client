f1f48790fdd7b50151543bd6983de6dbc918b873
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-19 14:21:45 -0500

    added idle message after timing out of game

--- play.js
+++ play.js
@@ -1,15 +1,20 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    // host: // IP address here,
-    // port: // PORT number here,
+    host: 'localhost',// IP address here,
+    port: 50541// PORT number here,
   });
 
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  
+  conn.setTimeout(5000);
+  conn.on('timeout', () => {
+    console.log("you ded cuz you idled");
+  });
 
   return conn;
 };
