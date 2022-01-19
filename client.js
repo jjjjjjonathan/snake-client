f25e9fef070f35411ab5ecebb89c4282ebc3dfc4
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-19 15:40:43 -0500

    ensured user input will control snake in game (not well yet)

--- client.js
+++ client.js
@@ -13,9 +13,7 @@ const connect = function() {
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write('Name: JMC');
-    // setInterval(() => {
-    //   conn.write('Move: up');
-    // }, 50);
+   
   });
 
   conn.setTimeout(5000);
--- input.js
+++ input.js
@@ -1,5 +1,8 @@
 const { stdin } = require('process');
-const setupInput = () => {
+
+let connection;
+const setupInput = conn => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,6 +13,20 @@ const setupInput = () => {
 
 const handleUserInput = () => {
   stdin.on('data', key => {
+    if (key === "w") {
+
+        connection.write('Move: up');
+
+    };
+    if (key === "a") {
+        connection.write('Move: left');
+    }
+    if (key === "s") {
+        connection.write('Move: down');
+    }
+    if (key === "d") {
+        connection.write('Move: right');
+    }
     if (key === '\u0003') {
       console.log("thanks for playing");
       process.exit();
